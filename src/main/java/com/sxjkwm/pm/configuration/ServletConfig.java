d456949090db13d765613a9ff7b431244ca6e39d
Chu Ningfan <chuqingning@163.com>
2022-04-11 23:03:50 +0800

    finished pattern file download servlet

--- src/main/java/com/sxjkwm/pm/configuration/ServletConfig.java
+++ src/main/java/com/sxjkwm/pm/configuration/ServletConfig.java
@@ -11,15 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
 
+import javax.activation.MimetypesFileTypeMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.URLEncoder;
 
 @Configuration
 public class ServletConfig {
@@ -33,7 +34,6 @@ public class ServletConfig {
         servletServletRegistrationBean.setOrder(0);
         return servletServletRegistrationBean;
     }
-
 
     private static class WxLoginServlet extends HttpServlet {
 
@@ -72,24 +72,55 @@ public class ServletConfig {
         }
     }
 
-    private static class FileDownloadServlet extends HttpServlet {
 
+    @Bean
+    public ServletRegistrationBean<PatternFileDownloadServlet> patternFileDownloadServlet(@Autowired PatternFileService patternFileService) {
+        ServletRegistrationBean<PatternFileDownloadServlet> servletServletRegistrationBean = new ServletRegistrationBean<>();
+        servletServletRegistrationBean.setUrlMappings(Lists.newArrayList("/pattern.do"));
+        servletServletRegistrationBean.setServlet(new PatternFileDownloadServlet(patternFileService));
+        servletServletRegistrationBean.setName("WxLoginServlet");
+        servletServletRegistrationBean.setOrder(1);
+        return servletServletRegistrationBean;
+    }
+
+    private static class PatternFileDownloadServlet extends HttpServlet {
         private final PatternFileService patternFileService;
-
-        public FileDownloadServlet(PatternFileService patternFileService) {
+        public PatternFileDownloadServlet(PatternFileService patternFileService) {
             this.patternFileService = patternFileService;
         }
-
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             String filePath = req.getParameter("filePath");
             File file = patternFileService.get(filePath);
+            String fileName = file.getName();
+            String type = new MimetypesFileTypeMap().getContentType(fileName);
             resp.reset();
             resp.setHeader("content-type", type + ";charset=utf-8");
             resp.setContentType("application/octet-stream;charset=UTF-8");
-            resp.addHeader("Content-Length", String.valueOf(targetFile.length()));
+            resp.addHeader("Content-Length", String.valueOf(file.length()));
             resp.setCharacterEncoding("UTF-8");
-
+            byte[] buff = new byte[1024];
+            BufferedInputStream bis = null;
+            OutputStream os;
+            try {
+                String encodeFileName = URLEncoder.encode(fileName, "UTF-8");
+                resp.setHeader("Content-Disposition", "attachment;filename=" + encodeFileName);
+                os = resp.getOutputStream();
+                bis = new BufferedInputStream(new FileInputStream(file));
+                int i = bis.read(buff);
+                while (i != -1) {
+                    os.write(buff, 0, buff.length);
+                    os.flush();
+                    i = bis.read(buff);
+                }
+            } finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
         }
     }
 
