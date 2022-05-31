2298d915eec9e17a40a93f0c509fd50a2a931f7f
Vic <chuqingning@163.com>
2022-05-31 08:47:56 +0800

    changed method name

--- src/main/java/com/sxjkwm/pm/auth/controller/LogoutController.java
+++ src/main/java/com/sxjkwm/pm/auth/controller/LogoutController.java
@@ -2,15 +2,10 @@ package com.sxjkwm.pm.auth.controller;
 
 import com.sxjkwm.pm.auth.service.LoginService;
 import com.sxjkwm.pm.common.RestResponse;
-import com.sxjkwm.pm.configuration.annotation.Valve;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/logout")
@@ -25,7 +20,7 @@ public class LogoutController {
 
 
     @GetMapping
-    public RestResponse<Boolean> validate() {
+    public RestResponse<Boolean> logout() {
         return RestResponse.of(loginService.logout());
     }
 
