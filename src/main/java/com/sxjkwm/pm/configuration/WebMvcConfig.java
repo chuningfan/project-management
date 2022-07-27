10b9abae5069c62af89a030ea484b6bac077c8d0
Vic <chuqingning@163.com>
2022-07-27 15:11:26 +0800

    optimized code

--- src/main/java/com/sxjkwm/pm/configuration/WebMvcConfig.java
+++ src/main/java/com/sxjkwm/pm/configuration/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.sxjkwm.pm.configuration;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -8,6 +9,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
@@ -21,18 +24,6 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Bean
     @Order(0)
     public CorsFilter corsFilter() {
-//        CorsConfiguration corsConfig = new CorsConfiguration();
-//        corsConfig.setAllowCredentials(true);
-//        corsConfig.setMaxAge(3600L);
-//        addAttr(corsConfig, "origin", "*");
-//        addAttr(corsConfig, "method", "*");
-//        addAttr(corsConfig, "header", "*");
-//        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-//        configSource.registerCorsConfiguration("/**", corsConfig);
-//        FilterRegistrationBean<CorsFilter> corsBean = new FilterRegistrationBean<>(new CorsFilter(configSource));
-//        corsBean.setName("crossOriginFilter");
-//        corsBean.setOrder(0);
-//        return corsBean;
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", buildConfig()); //注册
         return new CorsFilter(source);
@@ -46,28 +37,12 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         return corsConfiguration;
     }
 
-//    private void addAttr(CorsConfiguration corsConfig, String key, String values) {
-//        if (StringUtils.isBlank(values) || CorsConfiguration.ALL.equals(values)) {
-//            switch (key) {
-//                case "origin": corsConfig.addAllowedOrigin(CorsConfiguration.ALL); break;
-//                case "header": corsConfig.addAllowedHeader(CorsConfiguration.ALL); break;
-//                case "method": corsConfig.addAllowedMethod(CorsConfiguration.ALL); break;
-//                default: break;
-//            }
-//        } else {
-//            Iterable<String> valItrable = Splitter.on(",").split(values);
-//            Iterator<String> itr = valItrable.iterator();
-//            String value;
-//            while (itr.hasNext()) {
-//                value = itr.next();
-//                switch (key) {
-//                    case "origin": corsConfig.addAllowedOrigin(value); break;
-//                    case "header": corsConfig.addAllowedHeader(value); break;
-//                    case "method": corsConfig.addAllowedMethod(value); break;
-//                    default: break;
-//                }
-//            }
-//        }
-//    }
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("50MB"); //KB,MB
+        factory.setMaxRequestSize("50MB");
+        return factory.createMultipartConfig();
+    }
 
 }
